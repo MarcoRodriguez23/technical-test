58d3485d8d870aa02ea0bedc698412f63278d44e
Marco Rodriguez <cv5233@innovaccion.mx>
2023-02-09 16:24:06 -0600

    Add favicon

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -9,6 +9,7 @@
     <!-- Fonts and styles -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="icon" href="{{asset('images/icon.png')}}" type="image/x-icon">
   </head>
 
   <body>
