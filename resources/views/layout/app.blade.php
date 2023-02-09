38ceda85b70b37ecf89b348e7c5eaadeb73fd5e0
Marco Rodriguez <cv5233@innovaccion.mx>
2023-02-09 14:59:53 -0600

    Init login form
    I added the login form, it is only the base of the form

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,49 +1,54 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Uthis Credit - @yield('title')</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="{{asset('css/app.css')}}">
-    </head>
-    <body>
-      <div class=" border-bottom mb-4">
-        <header class="d-flex flex-wrap justify-content-center py-3 mb-2 container">
-          <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-            <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-            <span class="fs-4 fw-bold">UThis Credit</span>
-          </a>
-    
-          <ul class="nav nav-pills">
-            <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">About</a></li>
-          </ul>
-        </header>
+    <title>Uthis Credit - @yield('title')</title>
+      
+    <!-- Fonts and styles -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+  </head>
+
+  <body>
+
+    <div class=" border-bottom mb-4">
+      <header class="d-flex flex-wrap justify-content-center py-3 mb-2 container">
+        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+          <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+          <span class="fs-4 fw-bold">UThis Credit</span>
+        </a>
+  
+        <ul class="nav nav-pills">
+          <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+        </ul>
+      </header>
+    </div>
+
+    <main class="container-fluid main-container">
+      <!--div para colocar la imagen de la izquierda-->  
+      <div class="image"></div>
+
+      <!--div para la parte derecha, titulo y formularios-->  
+      <div>
+        <h1 class="mx-auto my-0 text-center">
+          @yield('title')
+        </h1>
+        <div class="p-4">
+          @yield('main')
+        </div>
       </div>
 
-      <main class="container-fluid main-container">
-          
-        <div class="image"></div>
+    </main>
 
-        <div>
-          <h1 class="mx-auto my-0 text-center">
-            @yield('title')
-          </h1>
-          <div class="p-4">
-            @yield('main')
-          </div>
-        </div>
+    <footer class="d-flex flex-wrap justify-content-center align-items-center py-2 mt-4 border-top">
+      <span class="mb-3 mb-md-0 text-muted">© 2023 UThis Credit, Inc</span>
+    </footer>
 
-      </main>
-
-      <footer class="d-flex flex-wrap justify-content-center align-items-center py-2 mt-4 border-top">
-        <span class="mb-3 mb-md-0 text-muted">© 2023 UThis Credit, Inc</span>
-      </footer>
-    </body>
+  </body>
 </html>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,3 +3,20 @@
 @section('title')
   Login
 @endsection
+
+@section('main')
+ <form action="" method="POST">
+  <div>
+    <label for="rfc">
+      RFC
+    </label>
+    <input 
+      id="rfc" 
+      type="text" 
+      placeholder="RFC"
+      name="rfc"
+    >
+  </div>
+  <input type="submit" value="Continuar">
+ </form>
+@endsection
