dbc19d7f103289e9faf2f97130da6907306ea6f7
Marco Rodriguez <cv5233@innovaccion.mx>
2023-02-09 14:45:20 -0600

    Add header, footer, and main container
    I added a main container where I will set 2 columns, the firts is an image
    and the second will be the main information like forms.

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -9,9 +9,41 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
     </head>
-    <body class="container-xl">
-      <h1>
-        @yield('title')
-      </h1>
+    <body>
+      <div class=" border-bottom mb-4">
+        <header class="d-flex flex-wrap justify-content-center py-3 mb-2 container">
+          <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+            <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+            <span class="fs-4 fw-bold">UThis Credit</span>
+          </a>
+    
+          <ul class="nav nav-pills">
+            <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+          </ul>
+        </header>
+      </div>
+
+      <main class="container-fluid main-container">
+          
+        <div class="image"></div>
+
+        <div>
+          <h1 class="mx-auto my-0 text-center">
+            @yield('title')
+          </h1>
+          <div class="p-4">
+            @yield('main')
+          </div>
+        </div>
+
+      </main>
+
+      <footer class="d-flex flex-wrap justify-content-center align-items-center py-2 mt-4 border-top">
+        <span class="mb-3 mb-md-0 text-muted">© 2023 UThis Credit, Inc</span>
+      </footer>
     </body>
 </html>
