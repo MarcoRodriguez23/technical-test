c20e8a9d303671bd44257bf9846ac3e8f0c2f636
Marco Rodriguez <cv5233@innovaccion.mx>
2023-02-09 13:39:09 -0600

    Add bootstrap
    I added bootstrap framework using npm install

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -9,7 +9,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
     </head>
-    <body>
+    <body class="container-xl">
       <h1>
         @yield('title')
       </h1>
