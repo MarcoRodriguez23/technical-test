37f214f2e3dbf7431cdc1897952fabf050197b25
Marco Rodriguez <cv5233@innovaccion.mx>
2023-02-09 22:15:49 -0600

    Add  content in oferta and datos view
    I added the first design content in oferta and datos views, these views will
     improve in design and content

--- resources/views/datos.blade.php
+++ resources/views/datos.blade.php
@@ -3,3 +3,32 @@
 @section('title')
     datos
 @endsection
+
+@section('main')
+<div>
+  <h3>Datos personales</h3>
+  <p>Nombre: nombre</p>
+  <p>Apellido paterno: apellido</p>
+  <p>Apellido materno: apellido</p>
+  <p>RFC: rfc</p>
+  <p>fecha de nacimiento: dd/mm/aaaa</p>
+  <p>ingresos: $$$$</p>
+  <p>egresos: $$$$</p>
+  <p>Num-dependientes: #</p>
+  <p>estado civil: estado</p>
+  <p>genero: genero</p>
+  <p>ultimo grado de estudios: escolaridad</p>
+</div>
+<div>
+  <h3>Domicilio</h3>
+  <p>calle: calle</p>
+  <p>num exterior: #</p>
+  <p>num interior: #</p>
+  <p>colonia: colonia</p>
+  <p>municipio: municipio</p>
+  <p>estado: estado</p>
+  <p>cp: cp</p>
+</div>
+<button class="btn btn-primary text-white">Actualizar domicilio</button>
+<button class="btn btn-primary text-white">Recibir oferta</button>
+@endsection
--- resources/views/oferta.blade.php
+++ resources/views/oferta.blade.php
@@ -3,3 +3,14 @@
 @section('title')
     oferta
 @endsection
+
+@section('main')
+<div>
+  <p>monto: monto</p>
+  <p>plazo: plazo</p>
+  <p>pago mensual: $$$$</p>
+  <p>tasa de interes: $$$</p>
+</div>
+<button class="btn btn-primary text-white">Aceptar oferta</button>
+<button class="btn btn-danger text-white">Rechazar oferta</button>
+@endsection
