6b7f28158eb9ba99e4e993568b3e905876ed7631
Marco Rodriguez <cv5233@innovaccion.mx>
2023-02-16 11:40:12 -0600

    Add validation on API
    Agregue la validación necesaria para que al consultar una API envie mensajes
    de error cuando se ingresa un RFC invalido o no existe algun registro

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -2,94 +2,118 @@
 
 namespace App\Http\Controllers;
 
+use App\DatosOferta;
 use App\DatosCliente;
 use App\DatosDomicilio;
-use App\DatosOferta;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class APIController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    // /**
+    //  * Display a listing of the resource.
+    //  *
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function index()
+    // {
+    //     //
+    // }
+
+    // /**
+    //  * Show the form for creating a new resource.
+    //  *
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function create()
+    // {
+    //     //
+    // }
+
+    // /**
+    //  * Store a newly created resource in storage.
+    //  *
+    //  * @param  \Illuminate\Http\Request  $request
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function store(Request $request)
+    // {
+    //     //
+    // }
+
+    // /**
+    //  * Display the specified resource.
+    //  *
+    //  * @param  int  $id
+    //  * @return \Illuminate\Http\Response
+    //  */
+    
+     public function show($rfc)
     {
-        //
+      // Validar el RFC
+      $validator = Validator::make(['rfc' => $rfc], [
+        'rfc' => 'required|alpha_num|max:10'
+      ]);
+      
+      if ($validator->fails()) {
+          return response()->json([
+              'error' => 'RFC invalido'
+          ], 400);
+      }
+      
+      // Consultar la persona por RFC
+      $rfc = strtoupper($rfc);
+      $cliente = DatosCliente::where('rfc',$rfc)->first();
+      
+      if ($cliente) {
+        $domicilio = DatosDomicilio::find($cliente->cliente_id);
+        $oferta = DatosOferta::find($cliente->cliente_id);
+        $array = [
+          'datos_cliente' => $cliente,
+          'datos_domicilio' => $domicilio,
+          'datos_oferta' => $oferta,
+        ];
+        return response()->json($array);
+
+      } else {
+        return response()->json([
+            'error' => 'RFC no registrado'
+        ], 404);
+      }
+
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+    // /**
+    //  * Show the form for editing the specified resource.
+    //  *
+    //  * @param  int  $id
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function edit($id)
+    // {
+    //     //
+    // }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+    // /**
+    //  * Update the specified resource in storage.
+    //  *
+    //  * @param  \Illuminate\Http\Request  $request
+    //  * @param  int  $id
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function update(Request $request, $id)
+    // {
+    //     //
+    // }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-      $cliente = DatosCliente::find($id);
-      $domicilio = DatosDomicilio::find($id);
-      $oferta = DatosOferta::find($id);
-      $array = [
-        'datos_cliente' => $cliente,
-        'datos_domicilio' => $domicilio,
-        'datos_oferta' => $oferta,
-      ];
-      return response()->json($array);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+    // /**
+    //  * Remove the specified resource from storage.
+    //  *
+    //  * @param  int  $id
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function destroy($id)
+    // {
+    //     //
+    // }
 }
